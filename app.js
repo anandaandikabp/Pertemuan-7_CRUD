c9d071b7bcfdabd4f26daa68a15130aa0d781b82
anandaandikabp <ananda.andika18@gmail.com>
2022-07-12 16:07:36 +0700

    restore file

--- app.js
+++ app.js
@@ -68,6 +68,32 @@ yargs.command({
     },
 });
 
+// fungsi edit data
+yargs.command({
+    command:'edit',
+    describe:'edit contact',
+    builder:{
+        name:{
+            describe: 'Contact Name',
+            demandOption: true,
+            type: 'string',
+        },
+        email:{
+            describe: 'Contact Email',
+            demandOption: false,
+            type: 'string',
+        },
+        mobile:{
+            describe: 'Contact Mobile Number',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv){
+        contact.answer(argv.name,argv.email,argv.mobile)
+    },
+});
+
 yargs.parse();
 
 
